5ace2e5e0bc7df6eea0f20ba5162346c0e976702
yudiptr <yudiputrasabri789@gmail.com>
2022-09-07 14:59:24 +0700

    tutorial 1

---
+++ wishlist/models.py
@@ -0,0 +1,7 @@
+# Create your models here.
+from django.db import models
+
+class BarangWishlist(models.Model):
+    nama_barang = models.CharField(max_length=50)
+    harga_barang = models.IntegerField()
+    deskripsi = models.TextField()
