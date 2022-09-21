2273a49bf04655692d12a309ef99a4a8e566baf0
yudiptr <94793172+yudiptr@users.noreply.github.com>
2022-09-22 06:44:47 +0700

    lab 3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,7 +3,10 @@ from wishlist.views import show_wishlist
 from wishlist.views import show_xml
 from wishlist.views import show_json
 from wishlist.views import show_xml_id
+from wishlist.views import register
 from wishlist.views import show_json_id
+from wishlist.views import logout_user
+from wishlist.views import login_user
 
 app_name = 'wishlist'
 
@@ -13,4 +16,7 @@ urlpatterns = [
     path('json/', show_json, name="show_json"),
     path('xml/<int:id>', show_xml_id, name='show_xml_id'),
     path('json/<int:id>', show_json_id, name="show_json_id"),
+    path('register/', register, name='register'),
+    path('logout/', logout_user, name='logout'),
+    path('login/', login_user, name='login'),
 ]
